a2940a6fd8e48602a5e473731f0a566f03c29c85
Nischal1510 <142976628+Nischal1510@users.noreply.github.com>
2024-04-01 10:03:57 +0000

    project start

---
+++ Project_code/script.js
@@ -0,0 +1,44 @@
+const container = document.getElementById('container1');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+function populateOptions() {
+    var categorySelect = document.getElementById("categorySelect");
+    var itemSelect = document.getElementById("itemSelect");
+
+    itemSelect.innerHTML = ""; // Clear existing options
+
+    if (categorySelect.value === "Python") {
+        var Python = ["Basics", "Loops", "Data types"];
+        Python.forEach(function (Python) {
+            var option = document.createElement("option");
+            option.text = Python;
+            option.value = Python.toLowerCase();
+            itemSelect.add(option);
+        });
+    } else if (categorySelect.value === "Java") {
+        var Java = ["Basics", "Operators", "OOPS"];
+        Java.forEach(function (Java) {
+            var option = document.createElement("option");
+            option.text = Java;
+            option.value = Java.toLowerCase();
+            itemSelect.add(option);
+        });
+    }
+}
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var category = document.getElementById("categorySelect").value;
+    var item = document.getElementById("itemSelect").value;
+    alert("Subject: " + category + "\ntopic: " + item);
+    // You can further process the selected values here
+});
